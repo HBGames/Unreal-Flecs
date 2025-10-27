81b5036905220b0e93c756864cd9c165a7d79efe
Ronald Burns <7757589+ronaldburns@users.noreply.github.com>
2025-10-27 00:21:46 -0500

    Delete Source/UnrealFlecsTests directory

--- Source/UnrealFlecsTests/Fixtures/FlecsWorldFixture.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Elie Wiese-Namir © 2025. All Rights Reserved.
-
-#include "FlecsWorldFixture.h"
-
-#if WITH_AUTOMATION_TESTS
-
-#endif // WITH_AUTOMATION_TESTS
--- Source/UnrealFlecsTests/Tests/Basic/FlecsUObjectComponentTests.cpp
+++
@@ -1,8 +0,0 @@
-﻿
-#include "Misc/AutomationTest.h"
-
-#if WITH_AUTOMATION_TESTS
-
-#include "Tests/FlecsTestTypes.h"
-
-#endif // WITH_AUTOMATION_TESTS
