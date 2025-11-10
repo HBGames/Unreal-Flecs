dea9a6a84230b1a1fb6a62c5a4aecdf078c7c7da
Ronald Burns <7757589+ronaldburns@users.noreply.github.com>
2025-11-09 19:30:09 -0600

    Add macro for defining std::hash specializations
    Introduces the DEFINE_STD_HASH macro in Hashing.h to simplify creating std::hash specializations using GetTypeHash for custom types.

---
+++ Source/FlecsLibrary/Public/Standard/Hashing.h
@@ -0,0 +1,15 @@
+﻿// Solstice Games © 2024. All Rights Reserved.
+
+#pragma once
+
+#define DEFINE_STD_HASH(x) \
+	template <> \
+	struct std::hash<x> \
+	{ \
+	public: \
+		inline std::size_t operator()(const x& Value) const noexcept \
+		{ \
+			return GetTypeHash(Value); \
+		} \
+		\
+	}; 
